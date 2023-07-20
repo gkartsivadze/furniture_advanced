e0af1b2f74d562671d091db6bd4d0bd579b4c3a7
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-07-20 15:42:23 +0400

    fix of name Home.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import App from './App.jsx'
-import Home from './directions/home.jsx'
+import Home from './directions/Home.jsx'
 
 import './index.scss'
 
