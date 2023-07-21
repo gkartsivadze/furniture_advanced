bd2226c21cf9cca2357b05eede4fed28b44ba48e
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-07-21 23:41:01 +0400

    Finish for today

--- src/components/ProductListSection.jsx
+++ src/components/ProductListSection.jsx
@@ -1,15 +1,17 @@
 import ProductCard from "./ProductCard"
 
 export default function ProductListSection({ name, data }) {
-    return (
-        <section id="product_section">
-        <h1>{name}</h1>
+  return (
+    <section className="product_section">
+       <h1>{name}</h1>
+       <div className="scroll_wrapper">
         <div className="scrollable_list">
           {
             data.map(product => <ProductCard key={product.id} id={product.id} type={product.type} image={product.image} name={product.name} price={product.price} />)
           }
         </div>
         <button className="btn bm">View collection</button>
-      </section>
-    )
+      </div>
+    </section>
+  )
 }
