54d6a6a8eb350bc495627a858d92f067481809d0
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-07-26 14:11:21 +0400

    Improved filtering selects

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,19 +1,20 @@
-import { useState } from "react"
-
 export default function Filter({ passFilter }) {
     return (
         <div id="filter">
             <select onChange={passFilter} name="category" id="category">
-                <option value="plant_pots">Plant pots</option>
-                <option value="ceramics">Ceramics</option>
+                <option value="any">Category</option>
+                <option value="a_category">Category A</option>
+                <option value="b_category">Category B</option>
             </select>
             <select onChange={passFilter} name="price" id="price">
+                <option value="any">Price</option>
                 <option value="0_150">0-150</option>
                 <option value="150_300">150-300</option>
             </select>
             <select onChange={passFilter} name="brand" id="brand">
-                <option value="plantius">Plantius</option>
-                <option value="sofius">Sofius</option>
+                <option value="any">Brand</option>
+                <option value="a_brand">Brand A</option>
+                <option value="b_brand">Brand B</option>
             </select>
         </div>
     )
